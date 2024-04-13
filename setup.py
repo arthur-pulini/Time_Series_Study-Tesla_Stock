2827a19f6a1eeb9d85c017d5ddcd4616ca63afab
arthur-pulini <arthurpulini@gmail.com>
2024-04-13 10:17:29 -0300

    Structuring the project.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This is a personal project with the objective of studying time series. How to process this type of data, what models are applied, how to analyze the data. This project is being guided by my FIAP professor, Vinícius Holanda Cavalcante.',
+    author='Arthur Eduardo Luna Pulini',
+    license='MIT',
+)
